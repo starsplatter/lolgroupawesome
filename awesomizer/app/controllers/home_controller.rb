b26335c9e4a1929bfa5987239b39d2b98068b375
Jenn Colt <jrc88@cornell.edu>
2014-10-30 13:19:14 -0400

    hook it up

--- awesomizer/app/controllers/home_controller.rb
+++ awesomizer/app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
  
         clnt = HTTPClient.new
         @aString = clnt.get_content("http://mjc12-dev.library.cornell.edu:3000/list")
-        @aResponse = eval(@aString)
-        @a = @aResponse['response']['docs']
+        
 
 end
 end
